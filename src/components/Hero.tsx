2c60d173443b74398705a994f1c34cdc8ed2fb08
adamfadrian <adamfadrian12@gmail.com>
2023-07-09 23:06:33 +0700

    change button color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -82,7 +82,7 @@ const Hero = () => {
                 {/* 2 buttons search and watch */}
                 <div className='grid grid-cols-1 lg:grid-cols-2 items-center w-full lg:gap-6 gap-3 '>
                     <ThemeProvider theme={theme}>
-                    <Button className='mx-auto lg:mx-0 w-[282px] gap-4 py-4 px-6 lg:py-[22px] lg:px-9 lg:w-[344px] rounded-xl flex items-center bg-signUp' variant='contained' color='steelBlue'>
+                    <Button className='mx-auto lg:mx-0 w-[282px] gap-4 py-4 px-6 lg:py-[22px] lg:px-9 lg:w-[344px] rounded-xl flex items-center ' variant='contained' color='steelBlue'  href="#contained-buttons">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                             <g clipPath="url(#clip0_4_487)">
                                 <path d="M15.5 14H14.71L14.43 13.73C15.63 12.33 16.25 10.42 15.91 8.39002C15.44 5.61002 13.12 3.39002 10.32 3.05002C6.09001 2.53002 2.53002 6.09001 3.05002 10.32C3.39002 13.12 5.61002 15.44 8.39002 15.91C10.42 16.25 12.33 15.63 13.73 14.43L14 14.71V15.5L18.25 19.75C18.66 20.16 19.33 20.16 19.74 19.75C20.15 19.34 20.15 18.67 19.74 18.26L15.5 14ZM9.50002 14C7.01002 14 5.00002 11.99 5.00002 9.50002C5.00002 7.01002 7.01002 5.00002 9.50002 5.00002C11.99 5.00002 14 7.01002 14 9.50002C14 11.99 11.99 14 9.50002 14Z" fill="#553B33" />
